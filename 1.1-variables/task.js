5c33aa287d78c41e57c2c6387ecfb7131838a285
Tanya Samarova <tasha.kostina@yandex.ru>
2020-06-25 15:15:25 +0300

    Решение задач 1,2,3

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -7,21 +7,33 @@
 
 function averageMark(a, g, p) {
 	
-    // реализуйте здесь задание №1
+	// реализуйте здесь задание №1
+	let algebra = a;
+    let geography = g;
+	let physics = p;
+	
+    let averageMarks = (a + g + p)/3;
     
-	return // ваш ответ;
+	return averageMarks;// ваш ответ;
 }
 
 function sayHello(userName) {
 	
     // реализуйте здесь задание №2
-    
-	return // ваш ответ;
+	let myName = userName;
+	let massege = `Привет, мир! Меня зовут ${myName}`;
+
+	return massege; // ваш ответ;
 }
 
 function calculateFormula() {
     
 	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+	let x = 2;
+	let y = 22;
+	let z = 0;
+
+	let result = x * y + 5 * z + x - 1;
+
+    return result; // ваш ответ;
 }
